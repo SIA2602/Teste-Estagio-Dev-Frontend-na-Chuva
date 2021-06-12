3f46b3cdcfab2bb6dc511159e90ac2fb4a2dd7be
Sia2602 <danilojose26@hotmail.com>
2021-06-12 17:37:21 -0300

    Adicionando Card de Resumo com o clamp-text ja implementado.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var idElementSelected = 'Trabalhos' //Item selecionado na pagina
 var idPgSelected = 'PageTrabalhos' //Id atual da pagina visivel
+var expandCardResumo = false //status do card resumo, se o mesmo esta expandido ou nao
 
 //Funcao usada para mostrar visualmente que uma opcao do menu foi clicada
 //idElement: id do menu clicado
@@ -26,4 +27,33 @@ function isSelectedPage(idPage) {
     document.getElementById(idPage).style.display = "block";
 
     idPgSelected = idPage;
+}
+
+//Funcao ultilizada para expandir card de resumo
+function cardExpand() {
+    if(expandCardResumo){
+        var styles = `
+        .ocultLinetext{
+            display: -webkit-box;
+            -webkit-line-clamp: 5;
+            -webkit-box-orient: vertical;  
+            overflow: hidden;
+        }`        
+    }
+
+    else{
+        var styles = `
+        .ocultLinetext{
+            display: -webkit-box;
+            -webkit-line-clamp: 1000;
+            -webkit-box-orient: vertical;  
+            overflow: hidden;
+        }`
+    }    
+    expandCardResumo = !expandCardResumo
+    
+    var styleSheet = document.createElement("style")
+    styleSheet.type = "text/css"
+    styleSheet.innerText = styles
+    document.head.appendChild(styleSheet)
 }
